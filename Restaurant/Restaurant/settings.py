0934b5d6e8c95e6c95adfd33fc36b54c66adc4e0
Honda <honda.muneer@gmail.com>
2024-05-19 08:34:33 +0300

    updating Setting

--- Restaurant/Restaurant/settings.py
+++ Restaurant/Restaurant/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a^k%_ag3@e!qi=@03%agiqln3qwc6@suj$@$^%)p1p4u&i^^ty'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
