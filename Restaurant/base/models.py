ed7acc1e4cf2d6fc820673f4a0f4602840e55db6
Honda <honda.muneer@gmail.com>
2024-05-19 07:24:13 +0300

    adding Models

--- Restaurant/base/models.py
+++ Restaurant/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import User 
 
 # Create your models here.
+
+class Table(models.Model):
+    code = models.CharField(max_length=100)
+    description = models.TextField(blank=True,null=True)
+    def __str__(self):
+        return self
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    reserv_date = models.DateTimeField("Date Of Reservation")
+    
+    def __str__(self):
+        return self
