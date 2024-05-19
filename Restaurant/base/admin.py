adfe42282696d26470f79ecdc9eff820ea8a3220
Honda <honda.muneer@gmail.com>
2024-05-19 08:12:46 +0300

    updating Models

--- Restaurant/base/admin.py
+++ Restaurant/base/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Table,Reservation
 
 # Register your models here.
+admin.site.register(Table)
+admin.site.register(Reservation)
--- Restaurant/base/models.py
+++ Restaurant/base/models.py
@@ -6,13 +6,14 @@ from django.contrib.auth.models import User
 class Table(models.Model):
     code = models.CharField(max_length=100)
     description = models.TextField(blank=True,null=True)
+
     def __str__(self):
-        return self
+        return self.code
 
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     reserv_date = models.DateTimeField("Date Of Reservation")
-    
+
     def __str__(self):
-        return self
+        return f'Reservation for {self.user.username} on {self.reserv_date}'
