c8cddca99cf08d12a40527a278e658a1ad526074
Honda <honda.muneer@gmail.com>
2024-05-22 07:19:43 +0300

    updating templates and views

--- Restaurant/base/urls.py
+++ Restaurant/base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path        
-from .views import tabel_views , reservation_views
+from .views import tabel_views , reservation_views 
 
 
 urlpatterns = [
-    path('', tabel_views.IndexView.as_view(), name='home'),
-    path('', reservation_views.CreateView.as_view(), name='reservation'),
+    path('home/', tabel_views.HomePageView.as_view(), name='home'),  # Home page URL
+    path('', tabel_views.ListView.as_view(), name='index'),
+    path('table/<int:pk>', tabel_views.DetailView.as_view(), name='table'),
+    path('reservation/', reservation_views.CreateView.as_view(), name='reservation'),
 ]
--- Restaurant/base/views/tabel_views.py
+++ Restaurant/base/views/tabel_views.py
@@ -1,11 +1,26 @@
-from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render,get_object_or_404
-from django.views.generic import ListView, CreateView
+from django.views.generic import TemplateView
+from django.views.generic import ListView, CreateView , DetailView
 from django.urls import reverse_lazy
-from base.models import Table
+from base.models import Table , Reservation
 
 # Create your views here.
-class CreateView(ListView):
+class ListView(ListView):
     model = Table
     template_name = 'base/index.html'
     context_object_name = 'tables'
+
+
+class DetailView(DetailView):
+    model = Table
+    template_name = 'base/table.html'
+    context_object_name = 'table'
+
+class HomePageView(TemplateView):
+    template_name = 'base/home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tables'] = Table.objects.all()  # Fetch all tables
+        context['reservations'] = Reservation.objects.all()  # Fetch all reservations
+        return context
