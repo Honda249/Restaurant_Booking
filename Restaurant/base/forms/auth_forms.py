d3426803ac58527e7ead170c0733049bf3495320
Honda <honda.muneer@gmail.com>
2024-05-28 04:20:18 +0300

    updating gunicorn&nginx

--- Restaurant/base/forms/auth_forms.py
+++ Restaurant/base/forms/auth_forms.py
@@ -4,3 +4,4 @@ from django.contrib.auth.forms import AuthenticationForm
 class CustomLoginForm(AuthenticationForm):
     username = forms.CharField(max_length=254, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}))
