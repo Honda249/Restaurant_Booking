ad8f4b725c8b49a487f5470f0718cca26d5e9b50
Honda <honda.muneer@gmail.com>
2024-06-02 18:10:53 +0300

    adding detail view

--- Restaurant/base/urls.py
+++ Restaurant/base/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 
     path('', tabel_views.HomePageView.as_view(), name='home'),  
-    path('home', tabel_views.ListView.as_view(), name='index'),
     path('table/<int:pk>', tabel_views.DetailView.as_view(), name='table'),
 
     path('reservation/', reservation_views.ReservationCreate.as_view(), name='reservation'),
+    path('reservation/<int:pk>', reservation_views.ReservationCreate.as_view(), name='table_reservation'),
     path('update-reservation/<int:pk>/', reservation_views.ReservationUpdate.as_view(), name='update_reservation'),
     path('delete-reservation/<int:pk>/', reservation_views.ReservationDelete.as_view(), name='delete_reservation'),
 
