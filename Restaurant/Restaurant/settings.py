e3eb4c9c90e92230a06d1f77835ed41e87fd44ac
Honda <honda.muneer@gmail.com>
2024-06-06 18:36:59 +0300

    updating email template and setting

--- Restaurant/Restaurant/settings.py
+++ Restaurant/Restaurant/settings.py
@@ -97,8 +97,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hissen.mohaned@gmail.com' 
-EMAIL_HOST_PASSWORD = 'lrutxwruenpeibol'  
+EMAIL_HOST_USER = '*************' 
+EMAIL_HOST_PASSWORD = '************'  
 DEFAULT_FROM_EMAIL = 'info@restaurant.com'  
 
 
